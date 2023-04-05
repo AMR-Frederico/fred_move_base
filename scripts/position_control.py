3a6ed99f47d463335f509be05a536c7cd7ffd21f
DinossauroBebado <guipsilva32@gmail.com>
2023-04-05 22:40:37 +0000

    fix import bug

--- scripts/position_control.py
+++ scripts/position_control.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # /user/bin/env python3
-from pid_control import PositionController
+
 
 import rospy
 import tf
@@ -18,65 +18,65 @@ from geometry_msgs.msg import Pose2D
 
 from time import time 
 
-## ------------------------------- class pid 
+# ------------------------------- class pid 
 
-# class PositionController: 
-#     def __init__(self, KP, KI, KD):
-#         self.KP = KP
-#         self.KD = KD 
-#         self.KI = KI
+class PositionController: 
+    def __init__(self, KP, KI, KD):
+        self.KP = KP
+        self.KD = KD 
+        self.KI = KI
 
-#         self.time = time()
-#         self.last_time = time()
-#         self.delta_time = 0
+        self.time = time()
+        self.last_time = time()
+        self.delta_time = 0
 
-#         self.error = 0
-#         self.last_error = 0
-#         self.delta_error = 0
+        self.error = 0
+        self.last_error = 0
+        self.delta_error = 0
 
-#         self.integral = 0
+        self.integral = 0
     
-#     def proporcional(self):
+    def proporcional(self):
 
-#         return self.KP * self.error
+        return self.KP * self.error
     
-#     def integrative(self): 
+    def integrative(self): 
 
-#         self.integral += self.error * self.delta_time
-#             #anti wind-up
-#         if(self.integral > 1.5 or self.integral < -1.5):
-#             self.integral = 0
-#         # print(self.integral)
-#         return self.integral * self.KI
+        self.integral += self.error * self.delta_time
+            #anti wind-up
+        if(self.integral > 1.5 or self.integral < -1.5):
+            self.integral = 0
+        # print(self.integral)
+        return self.integral * self.KI
 
-#     def derivative(self):
-#         self.delta_error = self.error - self.last_error
+    def derivative(self):
+        self.delta_error = self.error - self.last_error
 
-#         if(self.delta_error != 0):
-#             self.delta_error = self.delta_error/self.delta_time
-#         else:
-#             self.delta_error = 0
-#         return self.delta_error*self.KD
+        if(self.delta_error != 0):
+            self.delta_error = self.delta_error/self.delta_time
+        else:
+            self.delta_error = 0
+        return self.delta_error*self.KD
             
-#     def output(self, kp, ki, kd, error):
-#         self.KP = kp
-#         self.KI = ki
-#         self.KD = kd
+    def output(self, kp, ki, kd, error):
+        self.KP = kp
+        self.KI = ki
+        self.KD = kd
 
-#         self.error = error
+        self.error = error
 
-#         self.time = time()
-#         self.delta_time = self.time - self.last_time
+        self.time = time()
+        self.delta_time = self.time - self.last_time
 
-#         if (self.error != 0):
-#             output = self.proporcional() + self.integrative() + self.derivative()
-#         else: 
-#             output = self.proporcional() + self.derivative()
+        if (self.error != 0):
+            output = self.proporcional() + self.integrative() + self.derivative()
+        else: 
+            output = self.proporcional() + self.derivative()
         
-#         self.last_error = self.error
-#         self.last_time = self.time
+        self.last_error = self.error
+        self.last_time = self.time
 
-#         return output
+        return output
 
 ## ------------------------------------ end of class
 
