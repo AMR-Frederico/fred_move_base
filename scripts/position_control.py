a40489e7919be6d35fe24eedc998432dbde34790
DinossauroBebado <guipsilva32@gmail.com>
2023-04-10 13:12:50 +0000

    colocar classe no main file e mudança do theta

--- scripts/position_control.py
+++ scripts/position_control.py
@@ -1,4 +1,4 @@
-from pid_control import PositionController
+
 
 import rospy
 import math
@@ -6,32 +6,93 @@ import tf
 
 from std_msgs.msg import Bool, Float32
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist, Pose2D, Quaternion
+from geometry_msgs.msg import Twist, Pose2D, PoseStamped, Quaternion
+
+from time import time
+
+class PositionController: 
+    def __init__(self, KP, KI, KD):
+        self.KP = KP
+        self.KD = KD 
+        self.KI = KI
+
+        self.time = time()
+        self.last_time = time()
+        self.delta_time = 0
+
+        self.error = 0
+        self.last_error = 0
+        self.delta_error = 0
+
+        self.integral = 0
+    
+    def proporcional(self):
+
+        return self.KP * self.error
+    
+    def integrative(self): 
+
+        self.integral += self.error * self.delta_time
+            #anti wind-up
+        if(self.integral > 1.5 or self.integral < -1.5):
+            self.integral = 0
+        # print(self.integral)
+        return self.integral * self.KI
+
+    def derivative(self):
+        self.delta_error = self.error - self.last_error
+
+        if(self.delta_error != 0):
+            self.delta_error = self.delta_error/self.delta_time
+        else:
+            self.delta_error = 0
+        return self.delta_error*self.KD
+            
+    def output(self, kp, ki, kd, error):
+        self.KP = kp
+        self.KI = ki
+        self.KD = kd
+
+        self.error = error
+
+        self.time = time()
+        self.delta_time = self.time - self.last_time
+
+        if (self.error != 0):
+            output = self.proporcional() + self.integrative() + self.derivative()
+        else: 
+            output = self.proporcional() + self.derivative()
+        
+        self.last_error = self.error
+        self.last_time = self.time
+
+        return output
 
 # ----- constants pid controller -> linear 
-KP_linear = 1 #0.25
-KI_linear = 0.1 #0.01
-KD_linear = 0.1 #0
+KP_linear = 0.5 #0.25
+KI_linear = 0.1#0.01
+KD_linear = 0 #0
 
 # ----- constants pid controller -> angular 
-KP_angular = 1.1#6.3 #0.5
-KI_angular = 0.1#1
+KP_angular = 1#6.3 #0.5
+KI_angular = 0#1
 KD_angular = 0#0
 
 # ----- setpoints / goal (x, y, theta)
-goal_pose = Pose2D
+goal_pose = Pose2D()
 goal_pose.x = 0
 goal_pose.y = 0
 goal_pose.theta = 0
+goal_quaternion = Quaternion()
 
 # ----- current robot pose/orientation (x, y, theta)
-current_pose = Pose2D
-current_quaternion = Quaternion
+current_pose = Pose2D()
+current_quaternion = Quaternion()
 
 # ----- tolerance
-Tolerance_linear = 0.1          # in meters 
+Tolerance_linear = 0.3         # in meters 
 Tolerance_angular = math.pi/2     # in rad 
-Distance_to_goal = 0.20            # in meters, for the exit point
+Distance_to_goal = 0           # in meters, for the exit point
 
 # ----- machine states
 active_pid = True
@@ -83,9 +144,11 @@ def ki_angular_callback(msg):
 
 def setpoint_callback(msg): 
     global goal_pose
-    goal_pose.x = msg.x
-    goal_pose.y = msg.y 
-    goal_pose.theta = msg.theta
+    goal_pose.x = msg.pose.position.x
+    goal_pose.y = msg.pose.position.y 
+
+    goal_quaternion = msg.pose.orientation 
+    goal_pose.theta = tf.transformations.euler_from_quaternion([goal_quaternion.x, goal_quaternion.y, goal_quaternion.z, goal_quaternion.w])[2]
 
 def odom_callback(odom_msg): 
     global current_pose
@@ -106,22 +169,29 @@ def odom_callback(odom_msg):
     error_linear = math.hypot(delta_x, delta_y)
 
     # theta angle to reach the current goal 
-    theta_entrance = math.atan2(delta_x,delta_y) - current_pose.theta
+    print(delta_x == 0 or delta_y == 0)
+    if (delta_x == 0 or delta_y == 0): 
+        theta_entrance = 0
+    else:
+        theta_entrance = math.atan2(delta_x,delta_y) - current_pose.theta
 
     # ajust signal of the error_linear
     if (goal_pose.x < current_pose.x): 
         error_linear = - error_linear
     
     # if the robot is close enought to the goal, the error angular must consider de exit angle now
-    if (error_linear < Distance_to_goal): 
-        # theta angle for leave de currente goal and go to the next one
-        theta_exit = goal_pose.theta - current_pose.theta
-    else: 
-        theta_exit = 0
+    # if (error_linear < Distance_to_goal): 
+    #     print("correção theta de saida")
+    #     # theta angle for leave de currente goal and go to the next one
+    #     theta_exit = goal_pose.theta - current_pose.theta
+    # else: 
+        # theta_exit = 0
     
     # calcula o ângulo total e ajusta para o intervalo [-pi, pi]
-    error_angular = current_pose.theta + theta_entrance + theta_exit
-    error_angular = math.atan2(math.sin(error_angular), math.cos(error_angular))
+    error_angular = theta_entrance #+ theta_exit
+
+    # error_angular = current_pose.theta + theta_entrance + theta_exit
+    # error_angular = math.atan2(math.sin(error_angular), math.cos(error_angular))
 
     # msg for the error topic 
     error_angular_msg.data = error_angular
@@ -132,9 +202,9 @@ def odom_callback(odom_msg):
     error_linear_pub.publish(error_linear_msg)
 
     # while the robot is out the tolerance, compute PID, otherwise, send 0 
-    if (abs(error_linear) > Tolerance_linear and abs(error_angular) > Tolerance_angular):
+    if (abs(error_linear) > Tolerance_linear or abs(error_angular) > Tolerance_angular):
         vel_msg.angular.z = angular.output(KP_angular, KI_angular, KD_angular, error_angular)
-        vel_msg.angular.x = linear.output(KP_linear, KI_linear, KD_linear, error_linear)
+        vel_msg.linear.x = linear.output(KP_linear, KI_linear, KD_linear, error_linear)
     else: 
         vel_msg.linear.x = 0.0
         vel_msg.angular.z = 0.0 
@@ -145,9 +215,13 @@ def odom_callback(odom_msg):
         cmd_vel_pub.publish(vel_msg)
 
     # debug
-    print(goal_pose.x)
-    print(f"erro angular: {round(error_angular,3)} | error linear: {round(error_linear,3)} | vel_linear: {round(vel_msg.linear.x,3)} | vel_angular: {round(vel_msg.angular.z,0)}")
- 
+    print("CONTROL POSITION NODE -------------------------------------------------")
+    print(f"SETPOINT -> x:{goal_pose.x} y:{goal_pose.y} theta:{round(goal_pose.theta,2)}")
+    print(f"DELTA -> x:{round(delta_x,2)}  y:{round(delta_y,2)}")
+    print(f"THETA -> entrance:{round(theta_entrance,2)}  exit:{round(math.atan2(delta_x,delta_y),2)}")
+    print(f"ERROR -> linear:{round(error_linear,2)}  angular:{round(error_angular,2)}")
+    print(f"VELOCITY OUTPUT -> linear:{round(vel_msg.linear.x,2)}  angular:{round(vel_msg.angular.z,2)}")
+    print("\n")
 
 def controller_position():
     rospy.init_node('position_controller', anonymous=True)
@@ -155,7 +229,7 @@ def controller_position():
     rospy.Subscriber("/control/on",Bool,turn_on_controller_callback)
 
     rospy.Subscriber("/odom", Odometry, odom_callback)
-    rospy.Subscriber("/control/position/setup/goal", Pose2D, setpoint_callback)
+    rospy.Subscriber("/goal_manager/goal/current", PoseStamped, setpoint_callback)
 
     rospy.Subscriber("/control/position/setup/linear/kp", Float32, kp_linear_callback)
     rospy.Subscriber("/control/position/setup/linear/ki", Float32, ki_linear_callback)
